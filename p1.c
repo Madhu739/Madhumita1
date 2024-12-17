e4cd2ac3c5163f0af208a5d14ee06388fef52df7
Madhu739 <madhumitab.23.becs@acharya.ac.in>
2024-12-17 23:36:53 +0530

    '#'stashing done

--- p1.c
+++ p1.c
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
  hello da !hiii
   hajiii!
 =======
  hello da !hiii!how !why!
 >>>>>>> dcf7b9e (file commit 1.2)
+=======
+ hello da !hiii!how !why !kyuuuu !
+>>>>>>> 87fb148 (stashing done)
